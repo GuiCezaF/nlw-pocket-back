9ec1a58d24c0deaba2992fa3805f2a991fb93d3a
Guilherme Felipe <guilherme.felipe@retta.com.br>
2024-09-12 09:19:44 -0300

    feat: adicionando script helper para iniciar o projeto e o banco de dados

---
+++ scripts/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Iniciando o container Docker...\n"
+docker compose pull && docker compose build && docker compose up -d
+sleep 2
+if [ $? -eq 0 ]; then
+  echo -e "\nContainer iniciado com sucesso. ✅"
+  echo -e "\nMigrando dados e atualizando pacotes"
+
+  pnpm install
+
+  npx drizzle-kit migrate
+  if [ $? -eq 0 ]; then
+      echo -e "\nBanco de dados migrado com sucesso. ✅"
+    else
+      echo -e "\nErro ao migrar o banco de dados. ❌"
+    fi
+
+else
+  echo -e "\nFalha ao iniciar o container Docker. ❌"
+fi
